42a5b9f31d9e3cb12799702f494368bcff886771
muad <muad.dib.space@gmail.com>
2010-02-26 18:57:37 +0000

    Semigroup, Abelian and Monoid!

--- Abstract/Algebra.v
+++ Abstract/Algebra.v
@@ -32,3 +32,35 @@ reflexivity.
 Qed.
 
 ** **)
+
+Class Semigroup `(M : Magma) := {
+  associativity : forall a b c,
+    a & (b & c) == (a & b) & c
+}.
+
+Class Abelian `(M : Magma) := {
+  commutativity : forall a b,
+    a & b == b & a
+}.
+
+Class Monoid `(M : Magma) := {
+  identity ;
+  leftIdentity : forall x, identity & x == x ;
+  rightIdentity : forall x, x & identity == x
+}.
+
+Notation "'zero'" := (@identity Additive _ _ _).
+Notation "'one'" := (@identity Multiplicative _ _ _).
+
+(** tests **
+
+Theorem monoid_test (S : Setoid) `(A : Monoid Additive S) `(M : Monoid Multiplicative S) :
+  zero (x) one == zero (+) zero.
+intros.
+rewrite leftIdentity.
+rewrite rightIdentity.
+reflexivity.
+Qed.
+
+** **)
+
